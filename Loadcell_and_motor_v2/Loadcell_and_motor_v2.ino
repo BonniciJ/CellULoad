b58189c69c7a3c0b423d885ff6ccde602603bc76
BonniciJ <johnnybonnici@gmail.com>
2024-03-12 15:25:51 +0000

    Loadcell measurement fix

--- Loadcell_and_motor_v2/Loadcell_and_motor_v2.ino
+++ Loadcell_and_motor_v2/Loadcell_and_motor_v2.ino
@@ -124,7 +124,9 @@ void loop() {
       movementRemaining -= abs(stepSize);
 
       //delay to ensure slow compression at specified rate 
-      delay(timePermm*60000*stepSize);  //should really take away the time for the move function to complete
+      int timeDelay = timePermm*60000*stepSize; //time to do one step
+      timeDelay -= stepSize * 7.5 * 1000;    //correct for the time it takes to turn the motor
+      delay(timeDelay);  
     }
 
     
@@ -170,6 +172,7 @@ float measure() {
   float measurement = -1000;
 
   static boolean newDataReady = 0;
+  const int serialPrintInterval = 10; //increase value to slow down serial print activity
 
   // check for new data/start next conversion:
   if (LoadCell.update()) newDataReady = true;
@@ -177,7 +180,12 @@ float measure() {
   // get smoothed value from the dataset:
   if (newDataReady) {
   
-    float i = LoadCell.getData();
+    float i;
+    if (millis() > t + serialPrintInterval) {
+      i = LoadCell.getData();
+      newDataReady = 0;
+      t = millis();
+    }
     measurement = i;
 
     
