656204bd4d89068efe7efa7a2885aac0eb8fd3d3
lucas32ard <163067438+lucas32ard@users.noreply.github.com>
2024-03-12 17:17:53 +0000

    cut-off

--- PID_Code/pid_code/pid_code.ino
+++ PID_Code/pid_code/pid_code.ino
@@ -9,6 +9,10 @@ const float Kd = 0.001
 const float T = 0.05 // What's our sampling time? How reg are we reading force value from load cell
 const float tau = 0.1 // Find suitable cut-off frequency for filtering of high frequency noise
 
+// Set cut-off frequency at around 30/40Hz --> most noise affecting load cells is power frequency noise (~ 50/60 Hz) 
+
+// omega_0 = 1/tau so set tau = 1/40 = 0.025
+
 //// Need to define measurement (readout of load cell) + store prevMeasurement
 
 float prevError = 0
